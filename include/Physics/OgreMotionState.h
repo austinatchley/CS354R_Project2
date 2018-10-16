50eb52b585063ec778dd5025f6338189020a9ead
Austin Atchley <austinatchley@gmail.com>
2018-10-15 23:23:11 -0500

    Use world position in OgreMotionState

--- include/Physics/OgreMotionState.h
+++ include/Physics/OgreMotionState.h
@@ -14,8 +14,12 @@ namespace Physics
 class OgreMotionState : public btMotionState
 {
     public:
-    OgreMotionState(const btTransform& trans, Ogre::SceneNode* node) : mNode(node), mTrans(trans)
+    OgreMotionState(const btTransform& trans, Ogre::SceneNode* node) : mNode(node)
     {
+		Ogre::Vector3 position = mNode->_getDerivedPosition( );
+		Ogre::Quaternion quaternion = mNode->_getDerivedOrientation( );
+		mTrans.setOrigin(btVector3( position.x, position.y, position.z ) );
+		mTrans.setRotation(btQuaternion(quaternion.x, quaternion.y, quaternion.z, quaternion.w )); 
     }
 
     virtual ~OgreMotionState()
